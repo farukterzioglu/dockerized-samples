25e02b612fe6fed42ebb08022b89c23c73c67514
Faruk Terzioğlu <faruk.terzioglu@hotmail.com>
2018-07-21 01:37:00 +0300

    init

---
+++ src/engineApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace engineApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("engineApp is up");
+        }
+    }
+}
---
+++ src/streamer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace streamer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("streamer is up!");
+        }
+    }
+}
