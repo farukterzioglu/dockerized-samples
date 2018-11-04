97dcbdcfb7c23a7f8e17a92603087e5a8e82a051
Faruk Terzioğlu <faruk.terzioglu@hotmail.com>
2018-11-04 22:20:19 +0300

    init new app

---
+++ src/consoleAppNetCore/consoleAppNetCore/consoleAppNetCore.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace consoleAppNetCore.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
